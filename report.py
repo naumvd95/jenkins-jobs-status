05dd5a90c79c4e3937ab94a0761349113c2ac69c
Ruslan <ruslan.khozinov@gmail.com>
2016-05-05 15:45:10 +0400

    code-review comments were added
    Try to use list comprehensions if it improves readability.
    for example: 
    failed_jobs = [ job for job in all_jobs if 'red' in  job['colour'] ] 
    
    Need to extend functionality:
     - add simple args parsing
    e.g. 
    python report.py failed-jobs .build, .regression, 8.0.
    python report.py console .build
    python report.py health-report .regression.test

--- report.py
+++ report.py
@@ -2,6 +2,12 @@ import jenkins
 import pprint
 import csv
 PP=pprint.PrettyPrinter()
+
+# should be passed into through an env variable
+# e.g. JENKINS_HOST=...
+
+# jenkins_url should be splitted to JENKINS_HOST and JENKINS_PORT
+# remove the addr from code
 jenkins_url = 'http://172.18.170.69:8080/'
 server = jenkins.Jenkins(jenkins_url)
 job_instance = server.get_all_jobs()
@@ -14,15 +20,18 @@ for job in job_instance:
     alljobs+=1
     if job['color'] == 'red':
         name = job['name']
+        # "startwith" should be replaced by "in" (what about the ".regression" case ?)
+        # filter should be passed by script's args
         if name.startswith('9.0.build') or name.startswith('9.0.regression') or name.startswith('8.0.contrail') or name.startswith('7.0'):
             count+=1
             counter = '#'+str(count)
             names = job['name']
             print counter+' '+job['name']
             if len(server.get_job_info(job['name'])['healthReport'])>1:
+                # all string concatenation should be replaced by .format 
                 url = server.get_job_info(job['name'])['lastFailedBuild']['url']+'testReport'
                 print 'testReport\n'+url
-
+                # use .format to generate lines line that  
                 print '================================================================================================'
             else:
                 url = server.get_job_info(job['name'])['lastFailedBuild']['url']+ 'console'
