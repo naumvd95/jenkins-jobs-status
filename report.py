a358e099d2b38ccad765159d4bf205cf6f46d41f
naumvd95 <naumvd95@gmail.com>
2016-05-05 19:23:22 +0300

    report mark 2

--- report.py
+++ report.py
@@ -2,13 +2,10 @@ import jenkins
 import pprint
 import csv
 PP=pprint.PrettyPrinter()
-
-# should be passed into through an env variable
-# e.g. JENKINS_HOST=...
-
-# jenkins_url should be splitted to JENKINS_HOST and JENKINS_PORT
-# remove the addr from code
-jenkins_url = 'http://172.18.170.69:8080/'
+JENKINS_HOST='172.18.170.69'
+JENKINS_PORT='8080'
+args=['9.0.build','9.0.regression','.regression','8.0.contrail','7.0']
+jenkins_url = 'http://'+JENKINS_HOST+':'+JENKINS_PORT
 server = jenkins.Jenkins(jenkins_url)
 job_instance = server.get_all_jobs()
 count=0
@@ -16,26 +13,40 @@ alljobs=0
 csv_file = open('failedbuilds.csv', 'w')
 csvwriter = csv.writer(csv_file, delimiter='|',quoting=csv.QUOTE_MINIMAL)
 csvwriter.writerow(['count', 'name', 'url'])
+
 for job in job_instance:
     alljobs+=1
     if job['color'] == 'red':
         name = job['name']
-        # "startwith" should be replaced by "in" (what about the ".regression" case ?)
-        # filter should be passed by script's args
-        if name.startswith('9.0.build') or name.startswith('9.0.regression') or name.startswith('8.0.contrail') or name.startswith('7.0'):
-            count+=1
-            counter = '#'+str(count)
-            names = job['name']
-            print counter+' '+job['name']
-            if len(server.get_job_info(job['name'])['healthReport'])>1:
-                # all string concatenation should be replaced by .format 
-                url = server.get_job_info(job['name'])['lastFailedBuild']['url']+'testReport'
-                print 'testReport\n'+url
-                # use .format to generate lines line that  
-                print '================================================================================================'
-            else:
-                url = server.get_job_info(job['name'])['lastFailedBuild']['url']+ 'console'
-                print 'Console output,  testReport does not exist\n'+url
-                print '================================================================================================'
-            csvwriter.writerow([counter, names, url])
-print alljobs
+        for filtername in args:
+            if filtername in name:
+                count+=1
+                counter = '#'+str(count)
+                names = job['name']
+                print counter+' '+job['name']
+                if len(server.get_job_info(job['name'])['healthReport'])>1:
+                    url = server.get_job_info(job['name'])['lastFailedBuild']['url']+'testReport'
+                    failed_number = server.get_job_info(job['name'])['lastFailedBuild']['number']
+                    output = (server.get_build_console_output(job['name'], failed_number)).encode('utf-8')
+                    print 'testReport\n'+url
+                    print '-'*160
+                    print 'console output\n'+output
+                    print '=' * 160
+
+                else:
+                    url = server.get_job_info(job['name'])['lastFailedBuild']['url']+ 'console'
+                    failed_number=server.get_job_info(job['name'])['lastFailedBuild']['number']
+                    output = (server.get_build_console_output(job['name'],failed_number)).encode('utf-8')
+                    print ' testReport does not exist\n'+url
+                    print '-'*160
+                    print 'console output\n'+output
+                    print '='*160
+                job_file = open(job['name'],'w')
+                job_file.writelines(job['name'])
+                job_file.write('\n')
+                job_file.write(url)
+                job_file.write('\n')
+                job_file.write(output)
+                job_file.close()
+                csvwriter.writerow([counter, names, url])
+print "all checked jobs: " + str(alljobs)
